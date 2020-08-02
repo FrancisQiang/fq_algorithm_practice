dcf842202ae0ba3c9ab9240054c370ef8d6359fe
木雪 <lgq270955@alibaba-inc.com>
2020-08-02 16:00:26 +0800

    feat: 将二叉树展开为链表

---
+++ src/com/fq/lc/tree/二叉树展开为链表.java
@@ -0,0 +1,52 @@
+package com.fq.lc.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ *
+ * 例如，给定二叉树
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * 将其展开为：
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ * @author muXue
+ * @date 2020/8/2 15:17
+ */
+public class 二叉树展开为链表 {
+
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+}
