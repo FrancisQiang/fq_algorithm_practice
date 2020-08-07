ae3a7fb8cdaa300fbbf3c9f137fc4c71ace1e754
木雪 <lgq270955@alibaba-inc.com>
2020-08-07 17:39:32 +0800

    feat: 相同的树

---
+++ src/com/fq/lc/tree/相同的树.java
@@ -0,0 +1,25 @@
+package com.fq.lc.tree;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * @author muXue
+ * @date 2020/8/7 17:35
+ */
+public class 相同的树 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+
+}
