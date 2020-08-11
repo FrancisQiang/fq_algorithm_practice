5eb83cc2d382d9010b27a64ae93e76cb0bdf5990
木雪 <lgq270955@alibaba-inc.com>
2020-08-11 14:50:26 +0800

    feat: 构建乘积序列

---
+++ src/com/fq/lc/math/打印从1到最大的n位数.java
@@ -0,0 +1,16 @@
+package com.fq.lc.math;
+
+import java.util.List;
+
+/**
+ * @author muXue
+ * @date 2020/8/10 15:14
+ */
+public class 打印从1到最大的n位数 {
+
+    // 考虑大数 使用全排列
+    public List<String> printNumbers(int n) {
+        return null;
+    }
+
+}
