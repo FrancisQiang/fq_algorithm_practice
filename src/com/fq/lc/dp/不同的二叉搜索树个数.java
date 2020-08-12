9a59c52fad71bfbe850dc5d273836a803e78c6de
FrancisQ <2487049041@qq.com>
2020-08-12 22:40:05 +0800

    feat: 正则表达式匹配

--- src/com/fq/lc/dp/不同的二叉搜索树个数.java
+++ src/com/fq/lc/dp/不同的二叉搜索树个数.java
@@ -12,12 +12,12 @@ public class 不同的二叉搜索树个数 {
         dp[0] = 1;
         dp[1] = 1;
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= i; j++) {
-                dp[i] +
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
             }
         }
-
+        return dp[n];
     }
 
 }
