fae9661f87e12e8fce76c115af6ced82bb8e3080
FrancisQ <2487049041@qq.com>
2020-09-07 23:14:55 +0800

    feat:接雨水

---
+++ src/com/fq/lc/doublepointer/接雨水.java
@@ -0,0 +1,26 @@
+package com.fq.lc.doublepointer;
+
+/**
+ * @author lgq
+ * @date 2020/9/6
+ */
+public class 接雨水 {
+
+    public int trap(int[] height) {
+        int sum = 0;
+        int max_left = 0;
+        int[] max_right = new int[height.length];
+        for (int i = height.length - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            max_left = Math.max(max_left, height[i - 1]);
+            int min = Math.min(max_left, max_right[i]);
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+}
