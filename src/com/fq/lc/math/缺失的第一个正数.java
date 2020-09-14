64e1731b78fc4b2b9ad7b41806e30b6f5713257d
木雪 <lgq270955@alibaba-inc.com>
2020-09-14 20:41:48 +0800

    缺失的第一个正数

--- src/com/fq/lc/math/缺失的第一个正数.java
+++ src/com/fq/lc/math/缺失的第一个正数.java
@@ -1,25 +1,31 @@
 package com.fq.lc.math;
 
 /**
+ *
+ *
  * @author muXue
  * @date 2020/7/29 20:53
  */
 public class 缺失的第一个正数 {
 
-//
-//    public int firstMissingPositive(int[] nums) {
-//        int max = Integer.MAX_VALUE;
-//        for (int i = 0; i < nums.length; i++) {
-//            if (nums[i] > 0 && nums[i] < max) {
-//                max = nums[i];
-//            }
-//        }
-//        if (max > 1) {
-//            return 1;
-//        }
-//        for (int i = 0; i < nums.length; i++) {
-//
-//        }
-//    }
+    // 遍历一次数组把大于等于1的和小于数组大小的值放到原数组对应位置，
+    //然后再遍历一次数组查当前下标是否和值对应，如果不对应那这个下标就是答案，
+    //否则遍历完都没出现那么答案就是数组长度加1。
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
 
 }
